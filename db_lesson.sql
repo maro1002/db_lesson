f8869851574b53d5c219c25449ecdef60dadc93d
maro1002 <pipupu@gmail.com>
2025-02-18 19:28:52 +0900

    修正

--- db_lesson.sql
+++ db_lesson.sql
@@ -9,6 +9,11 @@ udated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 Q2
 alter table people add  department_id int unsigned;
 
+ask
+emailの直後にdepartment_idカラムを追加するためにはどんな記述が必要でしょうか?
+->
+alter table people add  department_id int unsigned after email;
+
 Q3
 insert into departments 
 (department_id, name)values
@@ -19,37 +24,39 @@ insert into departments
 (5, "情報システム");
 
 
-insert into people values
-(11, "a", "a@mail", 21, 1, default, default, 1),
-(12, "b", "b@mail", 24, 2, default, default, 1),
-(13, "c", "c@mail", 32, 1, default, default, 1),
-(14, "d", "d@mail", 23, 1, default, default, 2),
-(15, "e", "e@mail", 22, 2, default, default, 2),
-(16, "f", "f@mail", 31, 1, default, default, 2),
-(17, "g", "g@mail", 41, 1, default, default, 2),
-(18, "h", "h@mail", 42, 1, default, default, 3),
-(19, "i", "i@mail", 33, 2, default, default, 4),
-(20, "j", "j@mail", 23, 1, default, default, 1);
+insert into people 
+(person_id, name, email, age, gender)values
+(11, "a", "a@mail", 21, 1, 1),
+(12, "b", "b@mail", 24, 2, 1),
+(13, "c", "c@mail", 32, 1, 1),
+(14, "d", "d@mail", 23, 1, 2),
+(15, "e", "e@mail", 22, 2, 2),
+(16, "f", "f@mail", 31, 1, 2),
+(17, "g", "g@mail", 41, 1, 2),
+(18, "h", "h@mail", 42, 1, 3),
+(19, "i", "i@mail", 33, 2, 4),
+(20, "j", "j@mail", 23, 1, 1);
 
 
 
-insert into reports values
-(11, 11, "test content 1", 2012-12-15, 2012-11-15),
-(12, 12, "test content 2", 2014-11-15, 2021-11-12),
-(13, 14, "test content 3", 2013-12-15, 2022-11-14),
-(14, 13, "test content 4", 2015-11-15, 2022-11-11),
-(15, 12, "test content 5", 2022-12-15, 2025-11-11),
-(16, 14, "test content 6", 2022-11-15, 2025-11-21),
-(17, 15, "test content 7", 2023-12-25, 2025-11-22),
-(18, 14, "test content 8", 2023-11-13, 2025-11-22),
-(19, 13, "test content 9", 2024-11-22, 2025-11-23),
-(20, 11, "test content 10", 2012-11-15, 2012-11-15);
+insert into reports 
+(report_id, person_id, content)values
+(11, 11, "test content 1"),
+(12, 12, "test content 2"),
+(13, 14, "test content 3"),
+(14, 13, "test content 4"),
+(15, 12, "test content 5"),
+(16, 14, "test content 6"),
+(17, 15, "test content 7"),
+(18, 14, "test content 8"),
+(19, 13, "test content 9"),
+(20, 11, "test content 10"),
 
 Q4
 update people set department_id = 1 where department_id is null;
 
 Q5
-select name, age from people order by age desc;
+select name, age from people where gender = 1 order by age desc;
  
 Q6
 name、email、ageの三項目について
@@ -59,7 +66,7 @@ department_idが1のもの(=部署が開発のもの)に絞って
 
 
 Q7
-select name from people where age between 20 and 29 OR 40 and 49;
+select name from people where (gender = 2 and age between 20 and 29) OR (gender = 1 and age between 40 and 49);
 
 Q8
 select * from people where department_id = 1 order by age asc;
